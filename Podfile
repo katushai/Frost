1d2dd394e833306b73de3353f6641604c0e6fb43
katushai <blaine.anderson@gmail.com>
2014-02-14 01:06:16 -0500

    add objective sugar

--- Podfile
+++ Podfile
@@ -3,3 +3,4 @@ xcodeproj 'Frost/Frost.xcodeproj'
 
 pod 'FlurrySDK', '~> 4.3.2'
 pod 'Facebook-iOS-SDK', '~> 3.12.0'
+pod 'ObjectiveSugar', '~> 1.1.0'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -18,3 +18,9 @@
 #define COCOAPODS_VERSION_MINOR_FlurrySDK 3
 #define COCOAPODS_VERSION_PATCH_FlurrySDK 2
 
+// ObjectiveSugar
+#define COCOAPODS_POD_AVAILABLE_ObjectiveSugar
+#define COCOAPODS_VERSION_MAJOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_MINOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_PATCH_ObjectiveSugar 0
+
